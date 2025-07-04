822eb9b18186bef7e5cedd65a72341546741a4b6
munsifkhandev <munsifkhandev@gmail.com>
2025-07-04 21:01:36 +0500

    Understanding the Basics of Variables

---
+++ JS/JSBasics/variables.js
@@ -0,0 +1,11 @@
+let myName = "Munsif Khan";
+console.log(myName);
+
+let myAge = 22;
+console.log(myAge);
+
+let myloc = "Mianwali";
+console.log(myloc);
+
+let userLoggedIn = true;
+console.log(userLoggedIn);
