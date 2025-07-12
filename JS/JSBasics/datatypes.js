1c34917ab40d65f7a2138c293b44a965b859cb3d
munsifkhandev <munsifkhandev@gmail.com>
2025-07-13 01:00:37 +0500

    Basic Understanding of Datatypes

--- JS/JSBasics/datatypes.js
+++ JS/JSBasics/datatypes.js
@@ -6,3 +6,36 @@ console.log(myNumber);
 
 let numberValid = false;
 console.log(numberValid);
+
+let productName = "iPhone 16 Air";
+let specsProduct = "512GB Blue Color";
+let productPrice = 1500000;
+console.log(productName, specsProduct, productPrice);
+
+// Objects
+
+let myProduct = {
+  name: "Samsung",
+  price: 1500123,
+  isAvailable: false,
+};
+console.log(myProduct);
+
+// JavaScript Basics Revision:
+
+let stringValue = "Munsif Khan";
+let numberVal = 8039128309123;
+let userLoggedIn = true;
+
+let heheVal = null;
+
+let undefVal;
+
+console.log(stringValue, numberVal, userLoggedIn, heheVal, undefVal);
+
+let userInfo = {
+  name: "Munsif Khan",
+  age: 22,
+  CGPA: 3.25,
+};
+console.log(userInfo);
