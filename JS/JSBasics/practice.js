bfd3f65a2cad14361899286d8ca24860f7413d9b
munsifkhandev <munsifkhandev@gmail.com>
2025-07-13 18:46:27 +0500

    Added a Practice File for Code Revision

---
+++ JS/JSBasics/practice.js
@@ -0,0 +1,13 @@
+let myName = "Munsif Khan";
+
+let myAge = 22;
+
+let myCity = "Islamabad";
+let myCountry = "Pakistan";
+
+console.log(myName);
+console.log(myAge);
+console.log(myCity);
+console.log(myCountry);
+
+console.log("This is my Introduction: ", myName, myAge, myCity, myCountry);
