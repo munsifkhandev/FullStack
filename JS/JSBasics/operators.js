9393e43963accafeb5f3bc40ae08e0e4c56bb69b
munsifkhandev <munsifkhandev@gmail.com>
2025-07-13 17:50:52 +0500

    Understanding the Assignment Operators

--- JS/JSBasics/operators.js
+++ JS/JSBasics/operators.js
@@ -1,3 +1,5 @@
+// Arithmetic Operators :
+
 let x = 9;
 let y = 3;
 
@@ -8,4 +10,37 @@ console.log(x / y);
 console.log(x ** y);
 console.log(10 % 3);
 
-// Bas operators yahin tak hi tha.
+// Assignment Operations :
+
+//One way:
+
+let a = 10;
+let b = a + 5;
+
+console.log(b);
+
+// Second way :
+
+let p = 24;
+p = p + 22;
+
+console.log(p);
+
+//Third way :
+
+let q = 7;
+
+q += 9;
+console.log(q);
+
+let r = 15;
+r -= 5;
+console.log(r);
+
+let s = 20;
+s *= 2;
+console.log(s);
+
+let t = 100;
+t /= 4;
+console.log(t);
