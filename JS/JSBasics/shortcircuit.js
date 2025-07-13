3f43e90e755a2079f4ace2c4dc66d440b48410d8
munsifkhandev <munsifkhandev@gmail.com>
2025-07-14 01:17:57 +0500

    Understanding the concept of Short Circuiting

---
+++ JS/JSBasics/shortcircuit.js
@@ -0,0 +1,16 @@
+//Understanding Short-Circuit Evaluation in JavaScript:
+
+//Logical AND operators:
+
+console.log(10 && 20);
+console.log(0 && 20);
+console.log("" && null);
+console.log(0 && null);
+console.log(false && 90);
+
+// Logical OR operators:
+
+console.log(10 || "hehe");
+console.log("" || 29);
+console.log(undefined || null);
+console.log(true || 21);
