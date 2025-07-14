e781a19fa206be61d653184ca6b77df67e1c5668
munsifkhandev <munsifkhandev@gmail.com>
2025-07-14 18:15:03 +0500

    Understanding the Comparison operators

---
+++ JS/JSBasics/comparisonoperator.js
@@ -0,0 +1,16 @@
+// Understanding Camparison Operators:
+console.log(10 < 20);
+console.log(10 > 20);
+
+console.log(12 >= 12);
+console.log(76 <= 12);
+
+console.log(10 == 10);
+console.log(10 == "10");
+
+console.log(55 === 55);
+console.log("55" === 55);
+
+console.log(12 != 12);
+console.log(12 !== "12");
+
