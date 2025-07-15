d322c45b37dbd46411a08e22ffeeca468d1a2800
munsifkhandev <munsifkhandev@gmail.com>
2025-07-15 22:12:11 +0500

    Understanding IF-ELSE AND Nested IF-ELSE

---
+++ JS/JSBasics/conditionals/ifconditionals.js
@@ -0,0 +1,32 @@
+console.log("starting if conditionals");
+// Understanding If Conditionals in JavaScript:
+
+let isUserLoggedIn = true; // This variable indicates if the user is logged in or not
+
+if (isUserLoggedIn) {
+  console.log("Welcome back, user!"); // This message will be displayed if the user is logged in
+} else {
+  console.log("Please log in to continue."); // This message will be displayed if the user is not logged in
+}
+
+console.log("ending if conditionals");
+
+let myAge = 22;
+
+if (myAge > 18) {
+  console.log("I can vote");
+} else {
+  console.log("Sorryyy, you cant vote.");
+}
+console.log("End.....");
+
+console.log("............................................");
+
+let userSignedIn = true;
+let isPaidUser = true;
+
+if (userSignedIn == true && isPaidUser == true) {
+  console.log("Show Dashboard.....");
+} else {
+  console.log("Bhai Package krwao tabhi ana.");
+}
---
+++ JS/JSBasics/conditionals/nestedifelse.js
@@ -0,0 +1,15 @@
+console.log("Startinggggg................");
+
+let isUserSubscribed = true;
+let userHasLionsgatePlay = true;
+let userhasSonySubscription = true;
+
+if (isUserSubscribed) {
+  if (userHasLionsgatePlay) {
+    console.log("You can watch John Wick");
+  } else if (userhasSonySubscription) {
+    console.log("You can watch Sony Movies And Series");
+  } else {
+    console.log("You can watch free content only");
+  }
+}
