3499947273bcc6cea4e4d30466ebacdf681b3673
munsifkhandev <munsifkhandev@gmail.com>
2025-07-17 02:19:17 +0500

    Understanding the Bitwise Operators

---
+++ JS/JSBasics/bitwiseoperators.js
@@ -0,0 +1,26 @@
+let a = 5;
+let b = 6;
+
+// Bitwise AND
+let andResult = a & b; //
+console.log(andResult);
+// Bitwise OR
+let orResult = a | b; //
+console.log(orResult);
+// Bitwise XOR
+
+let xorResult = a ^ b; //
+console.log(xorResult);
+
+// Bitwise NOT
+let notResult = ~a; //
+console.log(notResult);
+console.log((bitwiseNotResult = ~b)); //
+
+// Left Shift
+let leftShiftResult = a << 1; //
+console.log(leftShiftResult);
+
+// Right Shift
+let rightShiftResult = a >> 1; //
+console.log(rightShiftResult);
