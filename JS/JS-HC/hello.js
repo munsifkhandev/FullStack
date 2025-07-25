cfc4809176b78a4f9bf32c858fa1b32324add6c5
munsifkhandev <munsifkhandev@gmail.com>
2025-07-26 04:26:52 +0500

    More on Javascript

--- JS/JS-HC/hello.js
+++ JS/JS-HC/hello.js
@@ -1,1 +1,33 @@
 console.log("Helloo Munsif Khan");
+
+function printChai() {
+  console.log("Chaii ly aao");
+}
+
+function paniLao() {
+  console.log("Pani ly aao yrr koi");
+}
+
+function bringBrush(numberOfBrush) {
+  console.log(`Jaldi se ${numberOfBrush} Brush ly ke aao`);
+}
+
+function addTwo(num1, num2) {
+  return num1 + num2;
+}
+addTwo(3, 4);
+console.log(addTwo(2, 4));
+paniLao();
+
+printChai();
+
+bringBrush(3);
+
+console.log(
+  `-----------------------------------------------------------------------------`
+);
+
+function cupcakes(trayOne, trayTwo) {
+  return trayOne + trayTwo;
+}
+console.log("Yeh hain total numbers of Cupcake: ", cupcakes(6, 6));
