9b054b3d760657ab806cc2caaab5bfdc43708467
munsifkhandev <munsifkhandev@gmail.com>
2025-07-27 03:10:04 +0500

    IF ELSE Practice set

---
+++ JS/JS-HC/pizza.js
@@ -0,0 +1,14 @@
+let numberofGuests = 5;
+let pizzaSize;
+
+// small <=2
+// medium <=5
+// large
+
+if (numberofGuests <= 2) {
+  console.log("Order Small Pizza......");
+} else if (numberofGuests <= 5) {
+  console.log("Bhaii Medium mangwa ly tera kaam hojae ga.....");
+} else {
+  console.log("Tum log Large nahi toh multiple Pizzas mangwa lo...");
+}
