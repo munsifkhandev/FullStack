3846afb86cfb18dcbc46da34d82b9aa0206757cc
munsifkhandev <munsifkhandev@gmail.com>
2025-07-27 04:17:17 +0500

    Understanding Datatypes in JS

---
+++ JS/JS-HC/bootcamp.js
@@ -0,0 +1,17 @@
+let myName = "Munsif Khan";
+let myAge = 22;
+let learningJS = true;
+let eatingSomething;
+let progessRN = null;
+let skills = ["HTML", "JS", "LIL BIT CSS"];
+console.log(myName, myAge, learningJS, eatingSomething, progessRN);
+console.log(skills);
+
+let studentInfo = {
+  name: "Munsif Khan",
+  age: 22,
+  height: "172 cm",
+  uni: "IIUI",
+  studentCurr: false,
+};
+console.log(studentInfo);
