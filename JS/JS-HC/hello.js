b0b9cfc9edc85bdddd40b670a040ea17509558aa
munsifkhandev <munsifkhandev@gmail.com>
2025-07-27 01:36:54 +0500

    Few Practice Questions on Functions

--- JS/JS-HC/hello.js
+++ JS/JS-HC/hello.js
@@ -31,3 +31,9 @@ function cupcakes(trayOne, trayTwo) {
   return trayOne + trayTwo;
 }
 console.log("Yeh hain total numbers of Cupcake: ", cupcakes(6, 6));
+
+function applesInBoxes(apples, boxes) {
+  return apples * boxes;
+}
+
+console.log("These are the number of apples in boxes:", applesInBoxes(12, 3));
