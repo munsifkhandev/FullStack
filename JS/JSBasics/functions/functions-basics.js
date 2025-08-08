350f5982e1f33691375c068a54fea5d43c165e6a
munsifkhandev <munsifkhandev@gmail.com>
2025-08-08 11:17:28 +0500

    Understanding Functions in JS

---
+++ JS/JSBasics/functions/functions-basics.js
@@ -0,0 +1,11 @@
+//using this Syntax we can easily define our Function / BlackBox
+
+function greetingsToEveryone() {
+  console.log("Starting.....");
+  console.log("Hello Jee , This is Line 1.....");
+  console.log("Hello Jee , This is Line 2.....");
+  console.log("Hello Jee , This is Line 3.....");
+  console.log("Ending.....");
+}
+
+greetingsToEveryone();
