9f36d3bf260e8f419a2ca56c73c97c25c5ba2957
munsifkhandev <munsifkhandev@gmail.com>
2025-08-09 12:48:05 +0500

    Find Even/Odd Num using If Else Question

---
+++ JS/JSBasics/conditionals/PracticeSets.js
@@ -0,0 +1,11 @@
+console.log("Welcome to the if else Practice Sets..");
+
+// Q) Given a number X , Check the value is Even or Odd??
+
+let number = 321;
+
+if (number % 2 == 0) {
+  console.log("It is a Even number...");
+} else {
+  console.log("It is a Odd Number...");
+}
