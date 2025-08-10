ba7bd54176c6a78901b8008c035b0301cec4056e
munsifkhandev <munsifkhandev@gmail.com>
2025-08-10 20:41:34 +0500

    Understanding Functions

--- JS/JSBasics/functions/functions-basics.js
+++ JS/JSBasics/functions/functions-basics.js
@@ -1,3 +1,5 @@
+console.log("...........Basic Functions..............");
+
 //using this Syntax we can easily define our Function / BlackBox
 
 function greetingsToEveryone() {
@@ -13,14 +15,23 @@ greetingsToEveryone();
 console.log(Math.sqrt(100));
 console.log(Math.sqrt(49));
 
-console.log(".........................");
+console.log("...........Single Argument/Parameter Functions..............");
 
 function isEvenOrOdd(x) {
+  //x is a parameter -> placeholder -> expect input
+
   if (x % 2 == 0) {
     console.log("Even Number.....");
   } else {
     console.log("Odd Number.....");
   }
 }
-isEvenOrOdd(12);
+isEvenOrOdd(12); // x = 12 , 12 is the Argument -> Actual input
 isEvenOrOdd(9);
+
+console.log("...........Multiple Argument/Parameter Functions..............");
+
+function multiply(a, b) {
+  console.log(a * b);
+}
+multiply(7, 4);
