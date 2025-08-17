552cc18cec4ac7a7f4217e76fd8ea79449c4c60c
munsifkhandev <munsifkhandev@gmail.com>
2025-08-17 20:00:09 +0500

    Basics of Array

---
+++ JS/JSBasics/arrays/array1.js
@@ -0,0 +1,6 @@
+let arr1 = [];
+let arr2 = [1, "munsif", true, "Hehe"];
+console.log(arr2);
+
+arr2[10] = "hehehe";
+console.log(arr2);
---
+++ JS/JSBasics/arrays/array2.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 4, "hehe"];
+console.log(arr.length);
+
+arr.push(21);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+arr.unshift("yeh bhi theak hai.");
+console.log(arr);
