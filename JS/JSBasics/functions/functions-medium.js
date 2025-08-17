843c6315829f8e81d7352e1c4dcc303e3eed4817
munsifkhandev <munsifkhandev@gmail.com>
2025-08-17 20:00:27 +0500

    calling function within function

--- JS/JSBasics/functions/functions-medium.js
+++ JS/JSBasics/functions/functions-medium.js
@@ -17,3 +17,15 @@ let s2 = "Hwlloo jee kya baat hai";
 let x = 1021211;
 let s3 = `What is this Number ${x}`;
 console.log(s3);
+
+console.log("-----------First citizen functions------------------");
+
+function someFunction(f1) {
+  f1();
+  console.log("Called function F1");
+}
+function helloJee() {
+  console.log("Helloo jeee ki haal hai");
+}
+
+someFunction(helloJee);
